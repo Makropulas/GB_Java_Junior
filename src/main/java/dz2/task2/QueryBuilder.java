0bbc16677c98160c7c9c69a3b41e7a01aef24650
DmitryMalomoshin <makropulas@gmail.com>
2024-01-15 19:05:39 +0300

    Added dz2.task2

---
+++ src/main/java/dz2/task2/QueryBuilder.java
@@ -0,0 +1,159 @@
+package dz2.task2;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class QueryBuilder {
+
+
+    /**
+     * Построить запрос на добавление данных в БД
+     *
+     * @param obj
+     * @return
+     */
+    public String buildInsertQuery(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        StringBuilder query = new StringBuilder("INSERT INTO ");
+
+        if (clazz.isAnnotationPresent(Table.class)) {
+            Table tableAnnotation = clazz.getAnnotation(Table.class);
+            query
+                    .append(tableAnnotation.name())
+                    .append(" (");
+
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    Column columnAnnotation = field.getAnnotation(Column.class);
+                    query
+                            .append(columnAnnotation.name())
+                            .append(", ");
+                }
+            }
+
+            if (query.charAt(query.length() - 2) == ',') {
+                query.delete(query.length() - 2, query.length());
+            }
+
+            query.append(") VALUES (");
+
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    field.setAccessible(true);
+                    query
+                            .append("'")
+                            .append(field.get(obj))
+                            .append("', ");
+                }
+            }
+
+            if (query.charAt(query.length() - 2) == ',') {
+                query.delete(query.length() - 2, query.length());
+            }
+
+            query.append(");");
+        } else {
+            return "";
+        }
+        return query.toString();
+    }
+
+    /**
+     * Построить запрос на получение данных из БД
+     *
+     * @param clazz
+     * @param primaryKey
+     * @return
+     */
+    public String buildSelectQuery(Class<?> clazz, UUID primaryKey) {
+        return queryFromTableWhereId("SELECT *", clazz, primaryKey);
+    }
+
+    /**
+     * Построить запрос на удаление данных из бд
+     *
+     * @param clazz
+     * @param primaryKey
+     * @return
+     */
+    public String buildDeleteQuery(Class<?> clazz, UUID primaryKey) {
+        return queryFromTableWhereId("DELETE", clazz, primaryKey);
+    }
+
+    public String queryFromTableWhereId(String startOfQuery, Class<?> clazz, UUID primaryKey) {
+        StringBuilder query = new StringBuilder(startOfQuery);
+        query.append(" FROM ");
+
+        if (clazz.isAnnotationPresent(Table.class)) {
+            Table tableAnnotation = clazz.getAnnotation(Table.class);
+            query.append(tableAnnotation.name()).append(" WHERE ");
+
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    Column columnAnnotation = field.getAnnotation(Column.class);
+                    if (columnAnnotation.primaryKey()) {
+                        query
+                                .append(columnAnnotation.name())
+                                .append(" = '").append(primaryKey).append("'");
+                        break;
+                    }
+
+                }
+            }
+            query.append(";");
+        } else {
+            return "";
+        }
+        return query.toString();
+    }
+
+    /**
+     * Построить запрос на обновление данных в бд
+     *
+     * @param obj
+     * @return
+     */
+    public String buildUpdateQuery(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        StringBuilder query = new StringBuilder("UPDATE ");
+
+        if (clazz.isAnnotationPresent(Table.class)) {
+            Table tableAnnotation = clazz.getAnnotation(Table.class);
+            query.append(tableAnnotation.name()).append(" SET ");
+            Field[] fields = clazz.getDeclaredFields();
+
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    field.setAccessible(true);
+                    Column columnAnnotation = field.getAnnotation(Column.class);
+                    if (columnAnnotation.primaryKey())
+                        continue;
+                    query.append(columnAnnotation.name()).append(" = '").append(field.get(obj)).append("', ");
+                }
+            }
+
+            if (query.charAt(query.length() - 2) == ',') {
+                query.delete(query.length() - 2, query.length());
+            }
+
+            query.append(" WHERE ");
+
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Column.class)) {
+                    Column columnAnnotation = field.getAnnotation(Column.class);
+                    if (columnAnnotation.primaryKey()) {
+                        query.append(columnAnnotation.name()).append(" = '").append(field.get(obj)).append("'");
+                        break;
+                    }
+                }
+            }
+            query.append(";");
+        } else {
+            return "";
+        }
+
+        return query.toString();
+    }
+}
